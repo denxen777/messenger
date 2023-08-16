414b9f416e9b111aa006144f8a677fd526b94184
denxen777 <denxen777@gmail.com>
2023-08-16 17:37:14 +0700

    commit 17:37

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Container, Stack } from '@mui/system';
 import { ChatList } from './components/chatList/ChatList';
 import { Messages } from './components/messages/Messages';
+import styles from './App.module.css';
 
 export const App = () => {
   return (
     <Container maxWidth={'xl'}>
-      <Stack direction='row' height={800}>
+      <Stack direction='row' className={styles.app}>
         <ChatList />
         <Messages />
       </Stack>
